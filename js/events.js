4c21a8fed890b4d2de58bd3bd1a17143af9d85af
jameseclement <jameseclement@gmail.com>
2018-12-19 16:19:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,9 +7,8 @@ function getIt() {
 
 
 function frameIt() {
-  
-  $('img').on('load', function(){
-  $('img').addClass('tasty')
+    $('img').on('load', function(){
+  $('img').addClass('tasty');
   })
 
 }
